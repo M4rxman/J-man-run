d16a5ec1c432f9da8428d2fb675b24a5849c4e8c
swarzeritter <32103493+swarzeritter@users.noreply.github.com>
2024-03-01 20:19:04 +0200

    added pre alpha

---
+++ modules/Module.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var level = $"../"
+var speed = 20
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.z += speed * delta
+	if position.z > 250:
+		level.spawnModule(position.z-(level.amount*level.offset))
+		queue_free()
