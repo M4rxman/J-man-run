1c52abade9b6683f62bcca637124569484fb3602
swarzeritter <32103493+swarzeritter@users.noreply.github.com>
2024-03-03 02:23:34 +0200

    a lot fixes

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody3D
 
 
-const SPEED = 13.0
+const SPEED = 17.0# 13
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var anim = $JESUS/AnimationPlayer
+@onready var animBoard = $JESUS/board/AnimationBoard
 @onready var death_sensor = $DeathSensor
 
 var score = 0
-
+var rotation_angle = 180
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,25 +21,45 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		anim.play("JESUS_JUMP1")
+		animBoard.play("BOARD_JUMP1")
 		
 	# Down jump
 	if Input.is_action_just_pressed("down") and !is_on_floor():
 		velocity.y = -JUMP_VELOCITY*2
+		anim.play("ArmatureAction")
+		animBoard.play("Armature_001Action")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
+	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		##velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		##velocity.z = move_toward(velocity.z, 0, SPEED)
+	var direction1 = Input.get_axis("ui_left", "ui_right")
+	
+	if direction1:
+		velocity.x = direction1 * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
 	
+	if direction1 == -1:
+		$JESUS.rotation.y = deg_to_rad(rotation_angle+10)
+	elif direction1 == 1:
+		$JESUS.rotation.y = deg_to_rad(rotation_angle-10)
+	else:
+		$JESUS.rotation.y = deg_to_rad(rotation_angle)
+		
 	score += 0.5
 	$"../CanvasLayer/HUD/Score".text = str(int(score))
 	move_and_slide()
+	if is_on_floor():
+		anim.play("ArmatureAction")
+		animBoard.play("Armature_001Action")
 	if death_sensor.is_colliding():
 		print("alarm")
 		death()
